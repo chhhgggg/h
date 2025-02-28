47adc120df776a3467e2de197104e61936b9e337
chhhgggg <unha3922@gmail.com>
2025-02-28 15:13:12 +0700

    Update kedotool.py

--- kedotool.py
+++ kedotool.py
@@ -23,6 +23,7 @@ if user_input in ['y', 'yes']:
     os.system('pip install dnspython')
     os.system('pip install tabulate')
     os.system('pip install bs4')
+    os.system('pip install rich')        
     os.system('pip install random_user_agent')
     os.system('pkg install android-tools')   
     print("Cài đặt hoàn tất. Vui lòng chạy lại chương trình!")
